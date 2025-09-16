53bd913b06d58d8356e51c812b94930e210c3ad5
Vinicius Testa Passos <viniciustestapassos@gmail.com>
2025-09-16 02:49:20 -0300

    feat: adiciona credencial de access_token

--- src/s3Service.js
+++ src/s3Service.js
@@ -7,12 +7,14 @@ const bucketName = process.env.AWS_S3_BUCKET_NAME;
 const region = process.env.AWS_REGION;
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+const sessionToken = process.env.AWS_SESSION_TOKEN;
 
 // Inicializa o cliente S3 com as credenciais
 const s3 = new S3({
     region,
     accessKeyId,
-    secretAccessKey
+    secretAccessKey,
+    sessionToken
 });
 
 // Função para fazer upload de um arquivo para o S3
